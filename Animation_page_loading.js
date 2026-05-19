b09ee05fcff5a6dbd83316c1a61148224955da2f
errornotjoin <joshuahastings29@gmail.com>
2026-05-19 13:45:03 +0100

    Refactor URL checks in loading page function for improved readability and maintainability

--- Animation_page_loading.js
+++ Animation_page_loading.js
@@ -27,14 +27,13 @@ async function Creating_elements_for_loading_page(what_JSON_file_to_read)
                     
             }
         })
-    //adding the animation   
 }
-
-if (location.href.includes("http://localhost/website/Contact.php") || location.href.includes("https://errornotjoin.tech/Contact.php") || location.href.includes("Contact.php") || location.href.includes("https://errornotjoin.tech/Contact.php"))
+var website_url = window.location.pathname
+if (website_url.endsWith("Contact.php"))
 {
     Creating_elements_for_loading_page("JSON/contact_page_information.json");
 }
-else if (location.pathname.includes("index.php")  || location.href.includes("https://errornotjoin.tech/index.php") ||  location.href.includes("http://localhost/website/index.php") || location.href.includes("https://errornotjoin.tech/") ||  location.href.includes("http://localhost/website"))
+else if (website_url == "" || website_url.endsWith("index.php"))
 {
     Creating_elements_for_loading_page("JSON/Home_page_information.json");
 }
